2535d2f9bf90d348590d4cb45b216a3f7f4f995e
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-05-15 20:51:25 +0800

    add: setTodos, fetch Todos from github API

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,8 +1,40 @@
+import useTodoStore from "@/store/todoStore";
+import { Todo } from "@/util/type";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { Button, HStack, IconButton, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  IconButton,
+  useColorMode,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const TopBar = () => {
+  const toast = useToast();
   const { colorMode, toggleColorMode } = useColorMode();
+  const [isFetchingTodos, setIsFetchingTodos] = useState(false);
+  const todoStore = useTodoStore();
+  const fetchTodos = async () => {
+    try {
+      setIsFetchingTodos(true);
+      fetch(
+        "https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json"
+      )
+        .then((resp) => resp.json())
+        .then((todos: Todo[]) => todoStore.setTodos(todos));
+    } catch (error) {
+      toast({
+        title: "Error fetching todos.",
+        status: "error",
+        position: "top",
+        isClosable: true,
+      });
+    } finally {
+      setIsFetchingTodos(false);
+    }
+  };
+
   return (
     <HStack spacing={2}>
       <IconButton
@@ -11,7 +43,14 @@ const TopBar = () => {
         icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
         onClick={toggleColorMode}
       />
-      <Button variant="minimal">Load JSON</Button>
+      <Button
+        isLoading={isFetchingTodos}
+        loadingText={"Fetching"}
+        onClick={fetchTodos}
+        variant="minimal"
+      >
+        Load JSON
+      </Button>
     </HStack>
   );
 };
